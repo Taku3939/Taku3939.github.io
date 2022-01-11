157002d3ad6b4a3adf99abd3b4034af2d233dbeb
Taku <sakisaki200012@gmail.com>
2022-01-11 15:38:45 +0900

    success

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -2,107 +2,110 @@ import React from "react";
 import Layout from "../components/Layout";
 //import Styles from "./Content.module.scss";
 //import imgUrl from "../imgs/holo.png";
-import { Row, Col, Container } from "react-bootstrap";
-import ContentCard, { CardParam } from "../components/ContentCard";
+import {Row, Col, Container} from "react-bootstrap";
+import ContentCard, {CardParam} from "../components/ContentCard";
 
 import ImgHolo from "../imgs/holo.png";
 import ImgArtNet from "../imgs/Art-Net.png";
 import ImgSp from "../imgs/spmodel-importer.png";
+import ImgMV2021 from "../imgs/mikuec2020virtual.png";
 
 const params: CardParam[] = [
-  {
-    title: "ArtNetReceiver",
-    main: "Unity上でArt-Net通信をするためのプラグインです。",
-    path: "https://github.com/Taku3939/ArtNetReceiver",
-    src: ImgArtNet.toString(),
-  },
-  {
-    title: "HoloRagnarok",
-    main: "ホロライブEnキャラクターによる格ゲー",
-    path: "/page/HoloRagnarok",
-    src: ImgHolo.toString(),
-  },
-  {
-    title: "SPModelImporter",
-    main: "SubstancePainterで出力したモデルを簡易にUnityに取り込むためのプラグイン",
-    path: "/page/artnet_receiver",
-    src: ImgSp.toString(),
-  },
-  {
-    title: "LiveCSharp",
-    main: "C#でP2P通信を行うことができるプロジェクト",
-    path: "https://github.com/Taku3939/LiveCSharp",
-    src: "",
-  },
-   {
-    title: "transferwise-ts",
-    main: "WiseのAPIをtypescriptで扱うためのプラグイン",
-    path: "https://github.com/Taku3939/transferwise-ts",
-    src: "",
-  },
-  
+    {
+        title: "ArtNetReceiver",
+        main: "Unity上でArt-Net通信をするためのプラグインです。",
+        gitUrl: "https://github.com/Taku3939/ArtNetReceiver",
+        src: ImgArtNet.toString(),
+    },
+    {
+        title: "HoloRagnarok",
+        main: "ホロライブEnキャラクターによる格ゲー",
+        // path: "/page/HoloRagnarok",
+        src: ImgHolo.toString(),
+    },
+    {
+        title: "SPModelImporter",
+        main: "SubstancePainterで出力したモデルを簡易にUnityに取り込むためのプラグイン",
+        gitUrl: "https://github.com/Astript/SPModelImporter",
+        src: ImgSp.toString(),
+    },
+    {
+        title: "LiveCSharp",
+        main: "C#でP2P通信を行うことができるプロジェクト",
+        gitUrl: "https://github.com/Taku3939/LiveCSharp",
+        src: "",
+    },
+    {
+        title: "transferwise-ts",
+        main: "WiseのAPIをtypescriptで扱うためのプラグイン",
+        gitUrl: "https://github.com/Taku3939/transferwise-ts",
+        src: "",
+    },
+    {
+        title: "MikuecVirtual 2021",
+        main: "VLLで作成した初音ミクのオンラインライブ",
+        src: ImgMV2021.toString(),
+    }
+
 ];
 
 const sliceByNumber = (array: any, n: number) => {
-  const length = Math.ceil(array.length / n);
+    const length = Math.ceil(array.length / n);
 
-  return new Array(length).fill(0).map((_, i) => {
-    return [array[i * n], array[i * n + 1], array[i * n + 2]];
-  });
+    return new Array(length).fill(0).map((_, i) => {
+        return [array[i * n], array[i * n + 1], array[i * n + 2]];
+    });
 };
 
 export default function Content() {
-  return (
-    <Layout>
-      <section className="py-5 text-center container">
-        <div className="row py-lg-5">
-          <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">My Works</h1>
-            <p className="lead text-muted">My programing works</p>
-          </div>
-        </div>
-      </section>
-
-      <div>
-        <Container>
-          {sliceByNumber(params, 3).map((p, index) => {
-            return (
-              <Row key={index}>
-                {p[0] && (
-                  <Col>
-                    <ContentCard
-                      title={p[0].title}
-                      main={p[0].main}
-                      path={p[0].path}
-                      src={p[0].src}
-                    />
-                  </Col>
-                )}
-                {p[1] && (
-                  <Col>
-                    <ContentCard
-                      title={p[1].title}
-                      main={p[1].main}
-                      path={p[1].path}
-                      src={p[1].src}
-                    />
-                  </Col>
-                )}
-                {p[2] && (
-                  <Col>
-                    <ContentCard
-                      title={p[2].title}
-                      main={p[2].main}
-                      path={p[2].path}
-                      src={p[2].src}
-                    />
-                  </Col>
-                )}
-              </Row>
-            );
-          })}
-        </Container>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <section className="py-5 text-center container">
+                <div className="row py-lg-5">
+                    <div className="col-lg-6 col-md-8 mx-auto">
+                        <h1 className="fw-light">My Works</h1>
+                        <p className="lead text-muted">My programing works</p>
+                    </div>
+                </div>
+            </section>
+            <Container>
+                {sliceByNumber(params, 3).map((p, index) => {
+                    return (
+                        <Row>
+                            {p[0] && (
+                                <Col md={4}>
+                                    <ContentCard
+                                        title={p[0].title}
+                                        main={p[0].main}
+                                        gitUrl={p[0].gitUrl}
+                                        src={p[0].src}
+                                    />
+                                </Col>
+                            )}
+                            {p[1] && (
+                                <Col md={4}>
+                                    <ContentCard
+                                        title={p[1].title}
+                                        main={p[1].main}
+                                        gitUrl={p[1].gitUrl}
+                                        src={p[1].src}
+                                    />
+                                </Col>
+                            )}
+                            {p[2] && (
+                                <Col md={4}>
+                                    <ContentCard
+                                        title={p[2].title}
+                                        main={p[2].main}
+                                        gitUrl={p[2].gitUrl}
+                                        src={p[2].src}
+                                    />
+                                </Col>
+                            )}
+                        </Row>
+                    )
+                })}
+            </Container>
+        </Layout>
+    );
 }
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,14 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
+import Styles from "./Top.module.scss";
+import { Container, Col, Row } from "react-bootstrap";
+
 export default function Top() {
   return (
     <div>
       <Layout>
-        <h2>hoge</h2>
-        <p>This is about page</p>
-        
-        <Link to="/about">about page</Link>
+        <div>
+          <div className={"container-fluid"}>
+            {/*タイトル*/}
+            <Row className={`${Styles.item} ${Styles.content_1}`}>
+              <Col className={"px-0"} xs={8}>
+                <h1 className={Styles.title}>Taku Portfolio</h1>
+                <hr/>
+                <div className={Styles.subTitle}>
+                  <p>C#, TypeScript, Python, C++</p>
+                  <p>すべてのうおうお、うおうお</p>
+                </div>
+              </Col>
+            </Row>
+            {/*自己紹介*/}
+            <Row className={`${Styles.item} ${Styles.content_2}`}>
+              {/*<Col>*/}
+              {/*  <div className={Styles.icon} />*/}
+              {/*</Col>*/}
+              <Col className ={Styles.left}>
+                <h2 className={Styles.title}>自己紹介</h2>
+                <p>名前 : Taku</p>
+                <p>
+                  Github :{" "}
+                  <a href="https://github.com/Taku3939">
+                    https://github.com/Taku3939
+                  </a>
+                </p>
+                <p>出身大学 : 電気通信大学</p>
+              </Col>
+            </Row>
+
+
+          </div>
+        </div>
       </Layout>
     </div>
   );
